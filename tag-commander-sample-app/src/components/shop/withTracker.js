aa47ca0f53e56a2f5b5b2f855fd064f73b7db424
Younes7 <younus.sayah@gmail.com>
2018-09-26 11:38:56 +0200

    enhanced router

---
+++ tag-commander-sample-app/src/components/shop/withTracker.js
@@ -0,0 +1,39 @@
+import React, { Component, } from "react";
+import TC_Wrapper from "react-tag-commander";
+
+const wrapper = TC_Wrapper.getInstance();
+
+const withTracker = (WrappedComponent, tcReloadOnly = {}) => {
+  
+  const trackPage = page => {
+    console.log(wrapper);
+    
+    wrapper.setTcVars(tcReloadOnly);
+  };
+
+  // eslint-disable-next-line
+  const HighOrderComponent = class extends Component {
+    componentDidMount() {
+      // eslint-disable-next-line
+      const page = this.props.location.pathname + this.props.location.search;
+      trackPage(page);
+    }
+
+    componentDidUpdate(prevProps) {
+      const currentPage = prevProps.location.pathname + prevProps.location.search;
+      const nextPage = this.props.location.pathname + this.props.location.search;
+
+      if (currentPage !== nextPage) {
+        trackPage(nextPage);
+      }
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
+  };
+
+  return HighOrderComponent;
+};
+
+export default withTracker;
