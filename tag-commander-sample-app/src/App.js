fd29603be8a6dc0c98d0183b99be4c3ad0ba8c54
Younes7 <younus.sayah@gmail.com>
2018-09-26 11:33:10 +0200

    enhanced router

--- tag-commander-sample-app/src/App.js
+++ tag-commander-sample-app/src/App.js
@@ -9,77 +9,8 @@ import Navbar from "./components/layout/navbar/Navbar";
 import Dashboard from "./components/dashboard/index.js";
 import Home from "./components/home/index.js";
 import Shop from "./components/shop/index.js";
-import withTracker from "react-tag-commander/tag-commander-sample-app/src/components/home/withTracker";
+import withTracker from "./components/shop/withTracker";
 
-// const Test = ({ routes }) => (
-//   <div>
-//     <h2>test</h2>
-//     <ul>
-//       <li><Link to="/test/test1">Test1</Link></li>
-//       <li><Link to="/test/test2">Test2</Link></li>
-//     </ul>
-//       {routes.map((route) => (
-//         <RouteWithSubRoutes key={route.path} {...route} />
-//       ))}
-//   </div>
-// );
-
-// const Test1 = () => <h3>Test1</h3>
-// const Test2 = () => <h3>Test2</h3>
-
-// const routes = [
-//   {
-//     path: '/home',
-//     component: Home,
-//     tcReloadOnly: [
-//       {ids :'4056', idc: '12'}
-//     ]
-//   },
-//   {
-//     path: '/shop',
-//     component: Shop,
-//     tcReloadOnly: [
-//       {ids :'4056', idc: '12'}, 
-//       {ids :'4056', idc: '11', options: 
-//         ["datastorage", "deduplication"]
-//       }
-//     ]
-//   },
-//   {
-//     path: '/test',
-//     component: Test,
-//     routes: [
-//       {
-//         path: '/test/cartItems',
-//         component: Test1
-//       },
-//       {
-//         path: '/test/cart',
-//         component: Test2
-//       }
-//     ]
-//   },
-//   {
-//     path: '/dashboard',
-//     component: Dashboard
-//   },
-//   {
-//     path: '',
-//     component: Home,
-//     tcReloadOnly: [
-//       {ids :'4056', idc: '12'}
-//     ]
-//   }
-// ];
-
-// const RouteWithSubRoutes = (route) => (
-//     <Route path={route.path} render={(props) => (
-//       <route.component {...props} 
-//       routes={route.routes} 
-//       />
-//     )}
-//     />
-// );
 
 const wrapper = TC_Wrapper.getInstance();
 wrapper.setDebug(true);
@@ -103,13 +34,17 @@ class App extends Component {
           <Navbar />
             <div className="container">
               <Switch>
-                {/* {
-                  routes.map(route => (
-                  <RouteWithSubRoutes key={route.path} {...route} />
-                  ))
-                } */}
-                <Route exact path="/home" component={Home} />
-                <Route exact path="/shop" component={withTracker(Shop, {options : "titi" })} />
+                <Route exact path="/home" component={withTracker(Home, 
+                {tcReloadOnly: [
+                  {ids :'4056', idc: '12'}
+                ]
+                })} />
+                <Route exact path="/shop" component={withTracker(Shop, 
+                {tcReloadOnly:[
+                  {ids :'4056', idc: '12'}, 
+                  {ids :'4056', idc: '11', options:["datastorage", "deduplication"]}
+                ] 
+                })} />
                 <Route exact path="/dashboard" component={Dashboard} />
               </Switch>
             </div>
