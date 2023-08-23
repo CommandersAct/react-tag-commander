1275b33b393acbf28bdfd10e93bbca04904dc3bd
Keno Dressel <hire@keno.digital>
2023-08-23 11:38:29 +0200

    awaits async container loading

--- tag-commander-sample-app/src/App.jsx
+++ tag-commander-sample-app/src/App.jsx
@@ -6,20 +6,30 @@ import Navbar from "./components/layout/navbar/Navbar";
 import Dashboard from "./components/dashboard/index.jsx";
 import Home from "./components/home/index.jsx";
 import Shop from "./components/shop/index.jsx";
+import { useEffect, useState } from "react";
 
 const wrapper = TC_Wrapper.getInstance();
 wrapper.setDebug(true);
-
-// to set the TagCommander container provide the id
-wrapper.addContainer('container_head', '/tag-commander-head.js', 'head');
-wrapper.addContainer('container_body', '/tag-commander-body.js', 'body');
-wrapper.removeContainer('container_body')
-
 function App() {
+
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    Promise.all([
+      wrapper.addContainer('container_head', '/tag-commander-head.js', 'head'),
+      wrapper.addContainer('container_body', '/tag-commander-body.js', 'body'),
+      wrapper.removeContainer('container_body')
+    ]).then(() => {
+      setIsReady(true);
+    });
+  }, []);
+
+
   return (
     <Router>
       <div className="App">
         <Navbar />
+        { isReady ?
         <div className="container">
           <Routes>
             <Route exact path="/" element={<Home />} />
@@ -27,7 +37,7 @@ function App() {
             <Route exact path="/dashboard" element={<Dashboard />} />
             <Route exact path="" element={<Home />} />
           </Routes>
-        </div>
+        </div> : <div className="container"><h1>Loading...</h1></div> }
       </div>
     </Router>
   );
